eb697845c55a1ca4c614455bceb4dca724786939
momo6266 <zhijing914@gmail.com>
2023-08-31 23:54:54 +0800

    dcs

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot
 import App from './App';
 import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
+//import awsconfig from './aws-exports';
+import config from "./aws-exports";
 
-Amplify.configure(awsconfig);
+Amplify.configure(config);
 
 const root = createRoot(document.getElementById('root')); // Create a root instance
 root.render(<App />);
